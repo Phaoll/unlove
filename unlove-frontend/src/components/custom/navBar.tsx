019d4a37a213f7d42f0bf558c25aa294e96898b1
Phaoll <paul.corradini@orange.fr>
2025-07-20 19:07:02 +0200

    FEAT: Navbar basics

--- unlove-frontend/src/components/custom/navBar.tsx
+++ unlove-frontend/src/components/custom/navBar.tsx
@@ -7,30 +7,44 @@ import {
   NavigationMenuList,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
+import { Button } from "../ui/button";
+
+// TODO
+// Implement light/dark theme
+// Implement language
+// Implement login
 
 export default function MainNavBar() {
   return (
     <nav className="border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container mx-auto flex h-16 items-center justify-between px-4">
-        <Link to="/" className="text-xl font-bold">
-          UnLove
-        </Link>
-        <NavigationMenu>
-          <NavigationMenuList className="flex space-x-2">
-            <NavigationMenuLink
-              asChild
-              className={navigationMenuTriggerStyle()}
-            >
-              <Link to="/">Home</Link>
-            </NavigationMenuLink>
-            <NavigationMenuLink
-              asChild
-              className={navigationMenuTriggerStyle()}
-            >
-              <Link to="/about">About</Link>
-            </NavigationMenuLink>
-          </NavigationMenuList>
-        </NavigationMenu>
+      <div className="container mx-auto flex h-16 items-center justify-between">
+        <div className="flex items-center gap-4">
+          <Link to="/" className="text-xl font-bold">
+            UnLove
+          </Link>
+          <NavigationMenu>
+            <NavigationMenuList>
+              <NavigationMenuLink
+                asChild
+                className={navigationMenuTriggerStyle()}
+              >
+                <Link to="/">Home</Link>
+              </NavigationMenuLink>
+              <NavigationMenuLink
+                asChild
+                className={navigationMenuTriggerStyle()}
+              >
+                <Link to="/about">About</Link>
+              </NavigationMenuLink>
+            </NavigationMenuList>
+          </NavigationMenu>
+        </div>
+
+        {/* <div className="flex gap-4">
+          <Button>Language</Button>
+          <Button>Dark/Light</Button>
+          <Button>Login</Button>
+        </div> */}
       </div>
     </nav>
   );
